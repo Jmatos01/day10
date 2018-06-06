2df07627524cffff2b34a0ab6571d96900508711
jmatos01 <jmatos@techlaunch.io>
2018-06-05 21:26:01 -0400

    first commit

---
+++ nodejs/routes/index.js
@@ -0,0 +1,13 @@
+//home route 
+exports.home = function(req, res){
+    res.render('home', {
+        title: "Home"
+    })
+    }
+    
+    //about route
+    exports.about = function(req, res){
+        res.render('about', {
+            title: "About Page"
+        })
+    }
